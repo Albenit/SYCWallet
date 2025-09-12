48418185f695b1e46d3a57758b0b30e0eca28bda
Albenit <albenit44@gmail.com>
2025-09-12 09:57:10 +0200

    first commit

---
+++ frontend/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import StartPage from './pages/StartPage'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <StartPage/>
+    </>
+  )
+}
+
+export default App
